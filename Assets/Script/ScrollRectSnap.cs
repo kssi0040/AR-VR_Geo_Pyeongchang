9169c01ad8b83e9011dae31d84af7f0b234cffe4
insix00-dev <heyriver@gmail.com>
2020-01-08 16:26:43 +0900

    2020.01.08. update
    Select Map Scene 에서
    Map 버튼 추가..

--- Assets/Script/ScrollRectSnap.cs
+++ Assets/Script/ScrollRectSnap.cs
@@ -158,4 +158,31 @@ public class ScrollRectSnap : MonoBehaviour
         SceneManager.LoadScene("Start");
     }
 
+
+    public void MapButtonEvent()
+    {
+        if (0 == iMinButtonNum)
+        {
+            //36.895005, 126.206617
+            string strUrl = "https://www.google.com/maps/place/%EB%8C%80%EA%B4%80%EB%A0%B9+%EC%82%BC%EC%96%91%EB%AA%A9%EC%9E%A5/@37.7227063,128.7180857,17z/data=!4m12!1m6!3m5!1s0x3561f6a6b0f28d7b:0x8dc24d7e4f39c48b!2z64yA6rSA66C5IOyCvOyWkeuqqeyepQ!8m2!3d37.72305!4d128.7196307!3m4!1s0x3561f6a6b0f28d7b:0x8dc24d7e4f39c48b!8m2!3d37.72305!4d128.7196307";
+            Application.OpenURL(strUrl);
+        }
+        else if (1 == iMinButtonNum)
+        {
+            string strUrl = "https://www.google.com/maps/place/%EC%9D%98%EC%95%BC%EC%A7%80%EC%B2%AD%EB%85%84%ED%9A%8C%EC%98%81%EB%86%8D%EC%A1%B0%ED%95%A9%EB%B2%95%EC%9D%B8/@37.6884764,128.7113911,17z/data=!3m1!4b1!4m5!3m4!1s0x3561f4c0dc562875:0x5920eeaf7ba4afc3!8m2!3d37.6884722!4d128.7135798";
+            Application.OpenURL(strUrl);
+        }
+        else if (2 == iMinButtonNum)
+        {
+            //36.845262, 126.196726            
+            //string strUrl = "https://www.google.com/maps/place/36.835972,126.195911";
+            //Application.OpenURL(strUrl);
+        }
+    }
+
+
+
+
+
+
 }
